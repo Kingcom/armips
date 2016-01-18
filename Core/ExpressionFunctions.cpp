38573f06133dc78db09e34fab81f9fa56f8b2292
Kingcom <sorgts@googlemail.com>
2016-01-19 00:16:01 +0100

    Add toHex and toString expression functions

--- Core/ExpressionFunctions.cpp
+++ Core/ExpressionFunctions.cpp
@@ -57,8 +57,52 @@ ExpressionValue expFuncFileSize(const std::vector<ExpressionValue>& parameters)
 	return result;
 }
 
+ExpressionValue expFuncToString(const std::vector<ExpressionValue>& parameters)
+{
+	ExpressionValue result;
+
+	switch (parameters[0].type)
+	{
+	case ExpressionValueType::String:
+		result.strValue = parameters[0].strValue;
+		break;
+	case ExpressionValueType::Integer:
+		result.strValue = formatString(L"%d",parameters[0].intValue);
+		break;
+	case ExpressionValueType::Float:
+		result.strValue = formatString(L"%g",parameters[0].floatValue);
+		break;
+	default:
+		return result;
+	}
+
+	result.type = ExpressionValueType::String;
+	return result;
+}
+
+ExpressionValue expFuncToHex(const std::vector<ExpressionValue>& parameters)
+{
+	ExpressionValue result;
+
+	if (parameters[0].isInt() == false || parameters.size() >= 2 && parameters[1].isInt() == false)
+	{
+		Logger::queueError(Logger::Error,L"Invalid parameter");
+		return result;
+	}
+
+	u32 digits = 8;
+	if (parameters.size() >= 2)
+		digits = (u32) parameters[1].intValue;
+
+	result.type = ExpressionValueType::String;
+	result.strValue = formatString(L"%0*X",digits,parameters[0].intValue);
+	return result;
+}
+
 const ExpressionFunctionMap expressionFunctions = {
 	{ L"endianness",	{ &expFuncEndianness,	0,	0 } },
 	{ L"fileexists",	{ &expFuncFileExists,	1,	1 } },
 	{ L"filesize",		{ &expFuncFileSize,		1,	1 } },
+	{ L"tostring",		{ &expFuncToString,		1,	1 } },
+	{ L"tohex",			{ &expFuncToHex,		1,	2 } },
 };
