Kingcom/armips

commit e3c4a5ae5a9e2d1cb8140cf0d612f10463b0b9bc

Cleanup includes in headers

Core/ExpressionFunctions.h
@@ -1,6 +1,12 @@
 #pragma once
-#include "Expression.h"
+
 #include <map>
+#include <memory>
+#include <vector>
+
+class Label;
+
+struct ExpressionValue;
 
 bool getExpFuncParameter(const std::vector<ExpressionValue>& parameters, size_t index, int64_t& dest,
 	const std::wstring& funcName, bool optional);

commit e6cde4849bc71a8bd72dbac066d4b9026fb216a3

Fix build on MSVC (probably broken in e3c4a5a).

Core/ExpressionFunctions.h
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <memory>
+#include <string>
 #include <vector>
 
 class Label;